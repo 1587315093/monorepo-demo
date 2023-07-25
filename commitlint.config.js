17b873772b4fb6a8229a3985d23c811e1714410b
jzh <1587315093@qq.com>
2023-07-25 14:58:35 +0800

    chore: init templete

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,5 +1,3 @@
 module.exports = {
-  // 继承了 @commitlint/config-conventional 规则，这是一个常见的规范配置，定义了常用的提交类型（feat、fix、docs、style、refactor、test、chore、revert）和一些约定。.
-  // commitlint 的文档：https://commitlint.js.org/#/reference-rules
   extends: ["@commitlint/config-conventional"],
 };
